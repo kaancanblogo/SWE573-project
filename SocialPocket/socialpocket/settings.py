07ee507ce68c9a6b27d976b2ebf76cf64cefec01
Kaan Can <sengunkaancan@gmail.com>
2023-01-02 23:59:59 +0300

    secret key is made public.

--- SocialPocket/socialpocket/settings.py
+++ SocialPocket/socialpocket/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = "django-insecure-jto%@6xavaytmb0-y*j8=pp*p7*ep06=aefc1fg5jd4w!(w)6l"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
